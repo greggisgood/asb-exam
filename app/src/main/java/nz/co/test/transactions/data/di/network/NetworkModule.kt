272efe235a7b0c835f8b831be5fa66c1d1df6234
Gregg Meyrick Jover <gmj@mega.co.nz>
2024-11-24 14:56:00 +1300

    Migrate Project from Dagger to Hilt
    Changelog:
    
    - Create MyApplication, which is an Application class to trigger Hilt code generation.
    
    - Declare MyApplication in AndroidManifest.xml and remove Dagger-related code.
    
    - Add @AndroidEntryPoint annotation in MainActivity.
    
    - Update NetworkModule with Hilt changes and initialize Retrofit, Moshi, and TransactionsService as Singleton objects.
    
    - Remove Dagger related classes.

--- app/src/main/java/nz/co/test/transactions/data/di/network/NetworkModule.kt
+++ app/src/main/java/nz/co/test/transactions/data/di/network/NetworkModule.kt
@@ -1,13 +1,36 @@
 package nz.co.test.transactions.data.di.network
 
+import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import nz.co.test.transactions.data.services.TransactionsService
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
 @Module
+@InstallIn(SingletonComponent::class)
 class NetworkModule {
+
     @Provides
-    fun providesTransactionService(retrofit: Retrofit): TransactionsService =
+    @Singleton
+    fun provideRetrofit(moshi: Moshi): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://gist.githubusercontent.com/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder().build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTransactionsService(retrofit: Retrofit): TransactionsService =
         retrofit.create(TransactionsService::class.java)
 }
